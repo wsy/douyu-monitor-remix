9502b82a8aae3e30560d76239aebd9f313dab454
qianjiachun <189964430@qq.com>
2022-03-23 14:49:44 +0800

    init

--- app/routes/$rid.tsx
+++ app/routes/$rid.tsx
@@ -1,13 +1,15 @@
 import { LinksFunction, LoaderFunction, useLoaderData } from "remix";
 import { getBagGiftData, getRoomGiftData } from "~/utils";
-import styleBase from "react-vant/es/styles/base.css";
+import styleVantBase from "react-vant/es/styles/base.css";
+import style from "~/styles/index.css";
 import { STT } from "~/utils/stt.js";
 import { Ex_WebSocket_UnLogin } from "~/utils/websocket.js";
 import { useEffect } from "react";
 
 export const links: LinksFunction = () => {
 	return [
-		{rel: "stylesheet", href: styleBase}
+		{rel: "stylesheet", href: styleVantBase},
+		{rel: "stylesheet", href: style}
 	]
 }
 
@@ -19,7 +21,6 @@ export const loader: LoaderFunction = async ({params}) => {
 	};
 	if (rid) {
 		// let roomId = await getRealRid(rid);
-		// console.log("哈哈", roomId)
 		let ret: any = await Promise.allSettled([getRoomGiftData(rid), getBagGiftData()]);
 		allGift.data = {...ret[0].value, ...ret[1].value};
 	}
@@ -36,7 +37,6 @@ const Index = () => {
 	const { rid, allGift } = useLoaderData();
 	// console.log(Ex_WebSocket_UnLogin);
 	useEffect(() => {
-		
 		let ws = new Ex_WebSocket_UnLogin(rid, (msg: string) => {
 			let data = stt.deserialize(msg);
 			console.log(data);
@@ -46,7 +46,8 @@ const Index = () => {
 	}, [rid]);
     return (
 		<>
-			<div>dsa</div>
+			<div className="haha"><span className="haha1">dsa</span></div>
+			<div className="haha1">我是2</div>
 		</>
     )
 }
